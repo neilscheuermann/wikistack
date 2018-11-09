f450f16f4906f01c036c01e356500f6a222ad69b
shawn michael larkin <shawn.michael.larkin@gmail.com>
2018-11-09 09:21:33 -0800

    up until schemas

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const volleyball = require('volleyball');
 const marked = require('marked');
+const layout = require('./views/layout');
+const { db } = require('./models');
 
 const app = express();
+
+db.authenticate().then(() => {
+  console.log('connected to the database');
+});
 
 app.use(volleyball);
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-  res.send('hello world');
+  const message = layout('hello world');
+  res.send(message);
 });
 
 const PORT = 3000;
