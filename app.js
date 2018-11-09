6d3e89a2c19001deefaef0a226fc2dd4880e2288
shawn michael larkin <shawn.michael.larkin@gmail.com>
2018-11-09 08:50:48 -0800

    setup sml-style

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const express = require('express');
-//Using volleyball instead of morgan.
 const volleyball = require('volleyball');
+const marked = require('marked');
 
 const app = express();
 
-app.use(volleyball("dev"));
-app.use(express.static(__dirname + "/public"));
+app.use(volleyball);
+app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-  res.send("hello world");
-})
+  res.send('hello world');
+});
 
 const PORT = 3000;
 
