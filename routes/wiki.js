91ab19bcb874968608b6e8c9d7d3c215461cbdcf
shawn michael larkin <shawn.michael.larkin@gmail.com>
2018-11-09 13:33:28 -0800

    up until showing a wiki page

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const { db, Page, User } = require('../models/index');
-const { addPage } = require('../views')
+const { addPage } = require('../views');
+
+function generateSlug(title) {
+  return title.replace(/\s+/g, '_').replace(/\W/g, '');
+}
 
 router.get('/', (req, res, next) => {
   res.send('nothing');
 });
 
-router.post('/', (req, res, next) => {
-  res.send(req.body);
+router.post('/', async (req, res, next) => {
+  const { title, content } = req.body;
+  const page = new Page({
+    title,
+    content,
+    slug: generateSlug(title),
+  });
+  try {
+    const returnedPage = await page.save();
+    console.log(returnedPage);
+    res.redirect('/');
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.get('/add', (req, res, next) => {
