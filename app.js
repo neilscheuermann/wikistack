e93b3ce8feb693fb8aafdcfca08dedb19d6c5f57
Neil Scheuermann <neilscheuermann@gmail.com>
2018-11-10 10:59:03 -0700

    Completed the assignment up to extra credit

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use(volleyball);
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: false }));
 app.use('/wiki', wikiRouter);
+app.use('/users', userRouter);
 app.get('/', (req, res) => {
   res.redirect('/wiki');
 });
--- routes/wiki.js
+++ routes/wiki.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { db, Page, User } = require('../models');
 const { addPage, wikiPage, main } = require('../views');
 
+// /wiki/
 router.get('/', async (req, res, next) => {
   try {
     const pages = await Page.findAll();
@@ -11,25 +12,39 @@ router.get('/', async (req, res, next) => {
   } catch (error) { next(error) }
 });
 
+// After clicking submit on form
 router.post('/', async (req, res, next) => {
-  const page = new Page(req.body);
   try {
+    const page = new Page(req.body);
+    const [user, wasCreated] = await User.findOrCreate({
+      where: {
+        name: req.body.name,
+        email: req.body.email
+      }
+    });
+
     await page.save();
+    page.setAuthor(user);
     res.redirect(`/wiki/${page.slug}`);
   } catch (error) { next(error) }
 });
 
+// /wiki/add
 router.get('/add', (req, res, next) => {
   res.send(addPage());
 });
 
+// wiki/:slug
 router.get('/:slug', async (req, res, next) => {
   try {
     const page = await Page.findOne({
       where: {slug: req.params.slug}
     })
-    console.log(page)
-    res.send(wikiPage(page))
+    const user = await User.findOne({
+      where: {id: page.authorId}
+    });
+    console.log(user)
+    res.send(wikiPage(page, user))
   } catch (error) { next(error) }
 })
 
